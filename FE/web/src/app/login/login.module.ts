c0ab9049f117de8a3a6e40493352520712c1e595
Swanand <swanandmehta94@gmail.com>
2022-07-02 15:12:39 +0530

    Created login components

--- FE/web/src/app/login/login.module.ts
+++ FE/web/src/app/login/login.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginMainComponent } from './component/login-main/login-main.component';
+import { LoginBoxComponent } from './component/login-box/login-box.component';
+import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginMainComponent,
+    LoginBoxComponent,
+    ResetPasswordComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
