ddb5bb7bea2f455a526a87219d62e457d5aeba12
Swanand <swanandmehta94@gmail.com>
2022-07-02 17:50:51 +0530

    Updating angular version

--- FE/web/src/app/auth/component/login/login.component.ts
+++ FE/web/src/app/auth/component/login/login.component.ts
@@ -4,24 +4,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginFormGroup: FormGroup;
 
   constructor(fb: FormBuilder) {
     this.loginFormGroup = fb.group({
-      "email": ['', [Validators.required, Validators.email]],
-      "password": ['', [Validators.required, Validators.minLength(3)]]
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login(): void {
-    console.log(this.loginFormGroup)
+    console.log(this.loginFormGroup);
   }
-
 }
