eb0c1dd8e57ee61e2e475cfc171d129b4f3e0875
Swanand <swanandmehta94@gmail.com>
2022-07-02 15:04:50 +0530

    Base web application

---
+++ FE/web/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './component/header/header.component';
+import { HeaderSearchComponent } from './component/header-search/header-search.component';
+import { HeaderNavigationComponent } from './component/header-navigation/header-navigation.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HeaderSearchComponent,
+    HeaderNavigationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
